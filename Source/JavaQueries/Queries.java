b23149c5bf2398e68670822a13a4c0d7cbbdbf76
Al <alexkhien@hotmail.com>
2020-11-23 20:46:29 -0500

    Completed queries up to 2.3

--- Source/JavaQueries/Queries.java
+++ Source/JavaQueries/Queries.java
@@ -70,5 +70,95 @@ public class Queries {
         }
     }
 
-    
+    //1.3 List all rooms, rooms occupied, by whom and admission dates for those occupied
+    public void allRoomsOccupation() {
+        String sql = "SELECT Rooms.roomNumber, currentInPatient.firstName, currentInPatient.lastName, "
+                + "currentInPatient.admissionDate "
+                + "FROM Rooms "
+                + "LEFT JOIN currentInPatient "
+                + "ON Rooms.roomNumber = currentInPatient.roomNumber;";
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql))   {
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("roomNumber") +  "\t"
+                        + rs.getString("firstName") + "\t"
+                        + rs.getString("lastName") + "\t"
+                        + rs.getString("admissionDate"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //2.1 List all patients in the database with all personal information
+    public void allPatients() {
+        String sql = "SELECT * "
+                + " FROM Patient;";
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql))   {
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("patientID") +  "\t"
+                        + rs.getString("firstName") + "\t"
+                        + rs.getString("lastName") + "\t"
+                        + rs.getString("primaryDoctorLastName"));
+                }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //2.2 List all currently admitted inpatients at the hospital
+    public void allCurrentPatients() {
+        String sql = "SELECT patientID, firstName, lastName "
+                + " FROM currentInPatient;";
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql))   {
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("patientID") +  "\t"
+                        + rs.getString("firstName") + "\t"
+                        + rs.getString("lastName"));}
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //2.3 List patients discharged within a given range
+    public void patientsWithinDischargeRange(String startDateIn, String lastDateIn) {
+
+        String sql = "SELECT patientID, firstName, lastName\n"
+                + " FROM InPatient "
+                + " WHERE dischargeDate "
+                + " BETWEEN '" + startDateIn + "' AND '" + lastDateIn + "';";
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql))   {
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("patientID") +  "\t"
+                        + rs.getString("firstName") + "\t"
+                        + rs.getString("lastName"));}
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //2.4
+
 }
--- Source/Menu.java
+++ Source/Menu.java
@@ -24,13 +24,30 @@ public class Menu {
       System.out.println("Please enter the query number to execute that query:");
       System.out.println();
 
-      //Right now we just need to EXECUTE the first query when the program is ran
+      //Connect to query
       Queries query = new Queries();
       query.connect();
+
       //1.1
       //query.roomUtilization();
 
       //1.2
-      query.roomOccupation();
+      //query.roomOccupation();
+
+      //1.3
+      //query.allRoomsOccupation();
+
+      //2.1
+      //query.allPatients();
+
+      //2.2
+      //query.allCurrentPatients();
+
+      //2.3
+//      String startDate = "2020-02-01";
+//      String lastDate = "2020-04-01";
+//      query.patientsWithinDischargeRange(startDate, lastDate);
+
+      //2.4
    }
 }
